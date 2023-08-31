d877aa05e9a67d6f6fad4fbed40b32786025cde8
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-08-31 16:17:32 -0300

    Estudando interface - inicio

---
+++ EstudoCsharpNelioAlves/EstudoCsharpNelioAlves/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace EstudoCsharpNelioAlves.Services
+{
+    public interface ITaxService
+    {
+    }
+}
---
+++ EstudoCsharpNelioAlves/EstudoCsharpNelioAlves/Services/RentalService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoCsharpNelioAlves.Services
+{
+    public class RentalService
+    {
+        public double Preçohora { get; set; }
+        public double Diaria { get; set; }
+
+        private ITaxService _taxService;
+
+        //rental service agora nao instanciara mais a classe dela e instanciara ITaxService
+        public RentalService(double preçohora, double diaria, ITaxService tax)
+        {
+            Preçohora = preçohora;
+            Diaria = diaria;
+            _taxService = tax;
+        }
+        public void ProcessInvoice(AluguelCarro aluguel)
+        {
+            TimeSpan duration = AluguelCarro.Finish.Subtract(aluguel.start);
+            if(duration.)
+        }
+    }
+}

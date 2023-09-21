60b045b346c2fd1420e17ecf09b0abc4e9207b14
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-09-21 11:27:02 -0300

    inclusao de estudo teste api via cep

---
+++ Apis/BlazorApp1/BlazorApp1/Pages/Model/ViaCepResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlazorApp1.Pages.Model
+{
+    public class ViaCepResponse
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+    }
+}
---
+++ EstudoCsharpNelioAlves/EstudoCsharpNelioAlves/Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+public class Pessoa : IComparable<Pessoa>
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public int CompareTo(Pessoa other)
+    {
+        // Comparar as pessoas com base na idade.
+        return Idade.CompareTo(other.Idade);
+    }
+
+    public override string ToString()
+    {
+        return $"{Nome} ({Idade} anos)";
+    }
+}

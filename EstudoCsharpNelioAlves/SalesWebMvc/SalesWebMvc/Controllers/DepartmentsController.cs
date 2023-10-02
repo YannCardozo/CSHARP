f578241b683a5ccab9f98b573243d865e61af6e1
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-10-02 12:09:36 -0300

    Criacao da index departments

--- EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
+++ EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
@@ -5,12 +5,13 @@ namespace SalesWebMvc.Controllers
 {
     public class DepartmentsController : Controller
     {
+
         public IActionResult Index()
         {
             List<Department> list = new List<Department>();
             list.Add(new Department { Id = 1, Name = "Eletronics" });
             list.Add(new Department { Id = 2, Name = "Fashion" });
-            return View();
+            return View(list);
         }
     }
 }

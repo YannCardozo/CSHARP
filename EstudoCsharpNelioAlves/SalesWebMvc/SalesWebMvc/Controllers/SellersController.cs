5ac5ddb279b4ce386da48440aea3df0cc586f87e
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-10-13 19:37:58 -0300

    Implementação delete e details do seller

--- EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
+++ EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Controllers/SellersController.cs
@@ -40,6 +40,43 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound("Não encontrado");
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
 
--- EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ EstudoCsharpNelioAlves/SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -25,6 +26,18 @@ namespace SalesWebMvc.Services
         {
 
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
+        }
+        public void Remove(int id)
+        {
+            _context.Seller.Include(obj => obj.Department).FirstOrDefault(x => x.Id == id);
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }

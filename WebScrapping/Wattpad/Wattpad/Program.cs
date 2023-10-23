9ecc4522af6461b618fcd18b584c00c2e871310a
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-10-23 17:55:38 -0300

    Subindo webscrapping exemplo wikipedia e example . com

---
+++ WebScrapping/Wattpad/Wattpad/Program.cs
@@ -0,0 +1,38 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Program
+{
+    public static void Main()
+    {
+        //Console.WriteLine("aaa");
+        HtmlWeb web = new HtmlWeb();
+
+        //Exemplo site example:
+
+        //HtmlDocument document = web.Load("https://example.com/");
+
+        //var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+        //var description = document.DocumentNode.SelectNodes("//div/p").First().InnerText;
+
+        //Console.WriteLine(title);
+        //Console.WriteLine();
+        //Console.WriteLine(description);
+
+        //Exemplo WIKIPEDIA:
+
+        HtmlDocument document = web.Load("https://pt.wikipedia.org/wiki/HTML");
+        var titulo = document.DocumentNode.SelectNodes("//*[@id=\"firstHeading\"]").First().InnerText;
+        var texto = document.DocumentNode.SelectNodes("//*[@id=\"mw-content-text\"]/div[1]/p");
+
+        Console.WriteLine(titulo);
+        Console.WriteLine();
+        texto.ToList().ForEach(i => Console.WriteLine(i.InnerText));
+            
+    }
+}
---
+++ WebScrapping/Wattpad/Wattpad/logindetails/LoginWattpad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.data
+{
+    public class LoginDetails
+    {
+
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public LoginDetails(string login, string email, string password)
+        {
+            Login = login;
+            Email = email;
+            Password = password;
+        }
+
+        //dados referentes ao WATTPAD
+        public LoginDetails()
+        {
+            Login = "UsernameTeste";
+            Email = "yann.cardozo@soulasalle.com.br";
+            Password = "Chaons26196460!@";
+        }
+
+
+
+
+
+
+
+    }
+}

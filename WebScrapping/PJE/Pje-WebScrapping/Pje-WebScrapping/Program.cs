8a5c2968920f8530ab8bb4c720e76d45a895c9ae
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-11-01 10:33:27 -0300

    correcao da senha errada

--- WebScrapping/PJE/Pje-WebScrapping/Pje-WebScrapping/Program.cs
+++ WebScrapping/PJE/Pje-WebScrapping/Pje-WebScrapping/Program.cs
@@ -13,30 +13,51 @@ public class Program
     public static void Main()
     {
 
-        LoginPage teste = new LoginPage();
-        teste.ExecuteLogin();
+        //LoginPage teste = new LoginPage();
+        //teste.ExecuteLogin();
 
 
-        //    IWebDriver Driver = new ChromeDriver();
-        //    Driver.Navigate().GoToUrl("https://tjrj.pje.jus.br/1g/login.seam");
+        //FUNCIONANDO PORRA PARABÉNS CARAIIIII!!!!
+
+        //PRoximo passo, validar o funcionamento com o login correto
+
+        //validando o funcionamento, reestruturar TOTALMENTE o projeto.
 
 
-        //    Thread.Sleep(4000);
 
-        //    // Espera até que o elemento de entrada do login esteja presente
-        //    var usernameInput = Driver.FindElement(By.Id("username"));
+        IWebDriver Driver = new ChromeDriver();
+        Driver.Navigate().GoToUrl("https://tjrj.pje.jus.br/1g/login.seam");
 
-        //    // Espera até que o elemento de entrada da senha esteja presente
-        //    var passwordInput = Driver.FindElement(By.Id("password"));
 
-        //    // Espera até que o botão de login esteja presente
-        //    var loginButton = Driver.FindElement(By.Name("login"));
+        Thread.Sleep(4000);
 
-        //    // Agora que os elementos estão presentes, você pode interagir com eles
-        //    usernameInput.SendKeys("Teste");
-        //    passwordInput.SendKeys("teste");
-        //    loginButton.Click();
-        //
+        Driver.SwitchTo().Frame(0);
+        IWebElement usernameInput = Driver.FindElement(By.Id("username"));
+        IWebElement passwordInput = Driver.FindElement(By.Id("password"));
+        IWebElement loginButton = Driver.FindElement(By.Name("login"));
+
+        usernameInput.SendKeys("15248945755");
+        passwordInput.SendKeys("Gabiroba22@");
+
+        loginButton.Click();
+
+        Thread.Sleep(4000);
+
+        Driver.Close();
+        //// Espera até que o elemento de entrada do login esteja presente
+        //var usernameInput = Driver.FindElement(By.Id("username"));
+
+        //// Espera até que o elemento de entrada da senha esteja presente
+        //var passwordInput = Driver.FindElement(By.Id("password"));
+
+        //// Espera até que o botão de login esteja presente
+        //var loginButton = Driver.FindElement(By.Name("login"));
+
+        //// Agora que os elementos estão presentes, você pode interagir com eles
+        //usernameInput.SendKeys("Teste");
+        //passwordInput.SendKeys("teste");
+        //loginButton.Click();
+
 
 
 

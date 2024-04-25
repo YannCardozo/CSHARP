bb13152cd27d80fbbf422bd1638595c38d1dba9e
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2024-04-25 15:05:04 -0300

    Incluindo tratamento de erros no login para senha errada

--- WebScrapping/PJE/Pje-WebScrapping/Pje-WebScrapping/Actions/Login/Login_PJE.cs
+++ WebScrapping/PJE/Pje-WebScrapping/Pje-WebScrapping/Actions/Login/Login_PJE.cs
@@ -14,28 +14,37 @@ namespace Pje_WebScrapping.Actions.Login
         private static IWebDriver driver;
         public static IWebDriver IniciarPJE(string url)
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
+            try
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl(url);
 
-            LoginModel LoginPJE = new LoginModel("15248945755", "Gabiroba22@", url);
-            Thread.Sleep(4000);
+                LoginModel LoginPJE = new LoginModel("15248945755", "Chokito22@", url);
+                Thread.Sleep(4000);
 
-            //esse comando ACESSSA o IFRAME dentro do html, que é um elemento dentro da tela de login que precisa ser acessado dessa forma.
-            driver.SwitchTo().Frame(0);
-            IWebElement usernameInput = driver.FindElement(By.Id("username"));
-            IWebElement passwordInput = driver.FindElement(By.Id("password"));
-            IWebElement loginButton = driver.FindElement(By.Name("login"));
+                //esse comando ACESSSA o IFRAME dentro do html, que é um elemento dentro da tela de login que precisa ser acessado dessa forma.
+                driver.SwitchTo().Frame(0);
+                IWebElement usernameInput = driver.FindElement(By.Id("username"));
+                IWebElement passwordInput = driver.FindElement(By.Id("password"));
+                IWebElement loginButton = driver.FindElement(By.Name("login"));
 
-            usernameInput.SendKeys(LoginPJE.Username);
-            Thread.Sleep(1000);
-            passwordInput.SendKeys(LoginPJE.Password);
-            Thread.Sleep(1000);
-            loginButton.Click();
+                usernameInput.SendKeys(LoginPJE.Username);
+                Thread.Sleep(1000);
+                passwordInput.SendKeys(LoginPJE.Password);
+                Thread.Sleep(1000);
+                loginButton.Click();
 
-            //esse comando SAI DO IFRAME e retorna para a tela normal do pje.
-            driver.SwitchTo().DefaultContent();
-            Console.WriteLine("Loguei no PJE");
-            return driver;
+                //esse comando SAI DO IFRAME e retorna para a tela normal do pje.
+                driver.SwitchTo().DefaultContent();
+                Console.WriteLine("Loguei no PJE");
+                return driver;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Erro no login PJE: " + ex.Message + " verificar credenciais do advogado.");
+                return driver;
+            }
+
         }
     }
 }
